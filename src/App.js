50bc7e63f4a601f6d39e19c08324798616027955
so_hell <armanyppph@gmail.com>
2023-09-18 11:47:29 +0530

    infinite scrolling without inputs

--- src/App.js
+++ src/App.js
@@ -1,47 +1,41 @@
-import React, { useState, useRef, useCallback } from 'react'
-import useBookSearch from './useBookSearch'
+import React, { useState, useRef, useCallback } from "react";
+import useBookSearch from "./useBookSearch";
 
 export default function App() {
-  const [query, setQuery] = useState('')
-  const [pageNumber, setPageNumber] = useState(1)
+  const [pageNumber, setPageNumber] = useState(1);
 
-  const {
-    books,
-    hasMore,
-    loading,
-    error
-  } = useBookSearch(query, pageNumber)
+  const { data, hasMore, loading, error } = useBookSearch(pageNumber);
 
-  const observer = useRef()
-  const lastBookElementRef = useCallback(node => {
-    if (loading) return
-    if (observer.current) observer.current.disconnect()
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasMore) {
-        setPageNumber(prevPageNumber => prevPageNumber + 1)
-      }
-    })
-    if (node) observer.current.observe(node)
-  }, [loading, hasMore])
-
-  function handleSearch(e) {
-    setQuery(e.target.value)
-    setPageNumber(1)
-  }
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore, pageNumber]
+  );
 
   return (
     <>
-      <input type="text" value={query} onChange={handleSearch}></input>
-      {books.map((book, index) => {
-        if (books.length === index + 1) {
-          return <div ref={lastBookElementRef} key={book}>{book}</div>
+      {data.map((book, index) => {
+        if (data.length === index + 1) {
+          return (
+            <div ref={lastBookElementRef} key={book}>
+              {book}
+            </div>
+          );
         } else {
-          return <div key={book}>{book}</div>
+          return <div key={book}>{book}</div>;
         }
       })}
-      <div>{loading && 'Loading...'}</div>
-      <div>{error && 'Error'}</div>
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "Error"}</div>
     </>
-  )
+  );
 }
-
--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -1,37 +1,36 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from "react";
 
-export default function useBookSearch(query, pageNumber) {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
-  const [books, setBooks] = useState([])
-  const [hasMore, setHasMore] = useState(false)
+// this is done to replicate the pagination
+
+const mimicPagination = async (pageNumber) => {
+  const itemsPerPage = 200;
+  const startIndex = (pageNumber - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  let data = await fetch("https://jsonplaceholder.typicode.com/photos");
+  data = await data.json();
+  const paginatedData = data.slice(startIndex, endIndex);
+  return { data: paginatedData, hasMore: pageNumber >= 5 ? false : true };
+};
+
+export default function useBookSearch(pageNumber) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
 
   useEffect(() => {
-    setBooks([])
-  }, [query])
+    console.log("FETHCING NEW DATA WITH PAGE NUMBER ", pageNumber);
+    setLoading(true);
+    setError(false);
 
-  useEffect(() => {
-    setLoading(true)
-    setError(false)
-    let cancel
-    axios({
-      method: 'GET',
-      url: 'http://openlibrary.org/search.json',
-      params: { q: query, page: pageNumber },
-      cancelToken: new axios.CancelToken(c => cancel = c)
-    }).then(res => {
-      setBooks(prevBooks => {
-        return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])]
-      })
-      setHasMore(res.data.docs.length > 0)
-      setLoading(false)
-    }).catch(e => {
-      if (axios.isCancel(e)) return
-      setError(true)
-    })
-    return () => cancel()
-  }, [query, pageNumber])
+    mimicPagination(pageNumber).then((res) => {
+      setData((prevBooks) => {
+        return [...new Set([...prevBooks, ...res.data.map((b) => b.title)])];
+      });
+      setLoading(false);
+      setHasMore(res.hasMore);
+    });
+  }, [pageNumber]);
 
-  return { loading, error, books, hasMore }
+  return { loading, error, data, hasMore };
 }
